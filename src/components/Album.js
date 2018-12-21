13726e24a3e803213083c2fe11bd3f08195f019d
Stephanie Fischler <sjfischler@gmail.com>
2018-12-21 15:00:10 -0500

    Complete Playback Checkpoint

--- src/components/Album.js
+++ src/components/Album.js
@@ -19,6 +19,31 @@ import albumData from './../data/albums';
 		this.audioElement.src = album.songs[0].audioSrc;
 	 }
 
+	 play() {
+		 this.audioElement.play();
+		 this.setState({ isPlaying: true });
+	 }
+
+	 pause() {
+		 this.audioElement.pause();
+		 this.setState({ isPlaying: false });
+	 }
+
+	 setSong(song) {
+		 this.audioElement.src = song.audioSrc;
+		 this.setState({ currentSong: song });
+	 }
+
+	 handleSongClick(song) {
+		 const isSameSong = this.state.currentSong === song;
+		 if (this.state.isPlaying && isSameSong) {
+			 this.pause();
+		 } else {
+			 if (!isSameSong) { this.setSong(song); }
+			 this.play();
+		 }
+	 }
+
    render() {
      return (
        <section className="album">
@@ -37,15 +62,13 @@ import albumData from './../data/albums';
 						<col id="song-duration-column" />
 					</colgroup>
 					<tbody>
-						{
-							this.state.song.map( (song, index) =>
-								<tr key={index}>
+             {this.state.album.songs.map( (song, index) =>
+               <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
 									<td>{index+1}</td>
 									<td>{song.title}</td>
 									<td>{song.duration}</td>
 								</tr>
-							)
-						}
+							)}
 					</tbody>
 				</table>
 			</section>
